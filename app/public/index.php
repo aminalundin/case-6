b536b8060f3a0c19832cb93e599b3460d2998062
aminalundin <lundinamina1@gmail.com>
2024-05-03 14:33:38 +0200

    first files created

---
+++ app/public/index.php
@@ -0,0 +1,15 @@
+<?php 
+$title = "Hello";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>make pour decisions</title>
+</head>
+<body>
+    <?php echo $title; ?>
+</body>
+</html>
