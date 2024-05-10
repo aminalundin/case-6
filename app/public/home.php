9ac1537270ad80c061e8791607efac4e49c0a649
aminalundin <lundinamina1@gmail.com>
2024-05-10 18:52:59 +0200

    started working on home.php page

--- app/public/home.php
+++ app/public/home.php
@@ -10,15 +10,33 @@ include "_includes/database_connection.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>make pour decisions</title>
-    <style>
+    <link rel="stylesheet" href="styles/styles-home.css">
+    <!-- <style>
         @font-face {
             font-family: Monsterrat;
             src: url(fonts/Montserrat-VariableFont_wght.ttf);
         }
-    </style>
+    </style> -->
 </head>
 <body>
-    <?php echo $title; ?>
+    <header>
+    <p class="slogan"><strike>MAKE POUR DECISIONS</strike></p>
+    </header>
+
+
+    <div class="top">
+        <input type="submit" class="search">
+
+    </div>
+
+    <div id="discover">
+
+    </div>
+
+    <footer>
+        <button class="sign_out">SIGN OUT</button>
+        <a href="">WHY POUR?</a>
+    </footer>
 
 </body>
 </html>
--- app/public/index.php
+++ app/public/index.php
@@ -7,15 +7,12 @@ include "_includes/database_connection.php";
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // echo "posted";
-    // print_r($_POST);
 
     $username_1 = $_POST["username_1"];
-    
 
     // validera inkommande data
     if (strlen($username_1) > 0 && strlen($username_1) < 50) {
-       
+
         // lägg till i databasen (från phpmyadmin)
         $sql = "INSERT INTO `users` (`username`) VALUES ('$username_1')";
         $result = $pdo->exec($sql);
@@ -23,20 +20,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } else {
         echo "skriv något";
     }
-
-    // echo $sql;
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // echo "posted";
-    // print_r($_POST);
 
-    
     $password_1 = $_POST["password_1"];
 
     // validera inkommande data
     if (strlen($password_1) > 0 && strlen($password_1) < 50) {
-       
+
         // lägg till i databasen (från phpmyadmin)
         $sql = "INSERT INTO `users` (`password`) VALUES ('$password_1')";
         $result = $pdo->exec($sql);
@@ -44,24 +36,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } else {
         echo "skriv något";
     }
-
-    // echo $sql;
 }
-// $id = 1;
-
-// PDO::prepare 
-// prepares a statement with named placeholder  - starts with a colon :
-// $sql = "SELECT * FROM `spring` WHERE id = :id";
-// $stmt = $pdo->prepare($sql);
-// $stmt->execute(['id' => $id]);
-// $rows = $stmt->fetchAll();
-
-// print_r($rows);
 
 ?>
 
 <style>
-/* 
+    /* 
 .video {
                 padding: 0;
                 margin: 0;
@@ -90,12 +70,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/styles-index.css">
 </head>
 
 <body>
 
-<!-- <div class="video">
+    <!-- <div class="video">
             <video autoplay muted loop>
                 <source src="styles/images/light.mp4" type="video/mp4">
             </video>
@@ -103,19 +83,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <!-- HEADER/HERO -->
     <?php include "_includes/header.php"; ?>
+    <p class="slogan"><strike>MAKE POUR DECISIONS</strike></p>
 
     <div class="hero">
         <img src="styles/images/pour-logotype-white.png" alt="pour-logotype" class="hero-logo">
-        <h1 class="hero-heading">WINE BAR INDEX</h1>
+       
 
         <p class="hero-content">DISCOVER YOUR NEW FAVOURITE BARS, SHARE
-YOUR RECOMMENDATIONS, 
-OWNER? 
-PUT YOURS ON THE LIST</p>
+            YOUR RECOMMENDATIONS,
+            OWNER?
+            PUT YOURS ON THE LIST</p>
     </div>
 
 
-    <p class="slogan"><strike>MAKE POUR DECISIONS</strike></p>
 
 
 
@@ -138,37 +118,29 @@ PUT YOURS ON THE LIST</p>
 
                 <label for="username_1">username</label>
                 <input type="text" name="username_1" id="username_1">
-                
+
                 <label for="password_1">password</label>
                 <input type="password" name="password_1" id="password_1">
-                <!-- <p>email</p>
-                <input type="text"><br>
-                <p>username</p>
-                <input type="text"><br>
-                <p>password</p>
-                <input type="password"><br>
-                <p>confirm password</p>
-                <input type="password2"> -->
                 <button type="submit" id="sign-up">SIGN UP!</button>
             </form>
         </div>
-        </div>
-        
+    </div>
 
-        <div class="log-in-container">
+
+    <div class="log-in-container">
         <div id="form-log-in" style="display: none;">
             <button onclick="displayLogInForm()" id="close-log-tab">&#x2715;</button>
             <form action="index.php" method="post">
                 <label for="username_2">username</label>
                 <input type="text" name="username_2" id="username_2">
-                
+
                 <label for="password_2">password</label>
                 <input type="password" name="password_2" id="password_">
             </form>
             <button type="submit" id="log-in">log in</button>
         </div>
-        </div>
-    
+    </div>
+
 
     <script src="script.js"></script>
 </body>
