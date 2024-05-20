6c112d78c7e599b1d4a37c0b4c2f9ac224489e94
aminalundin <lundinamina1@gmail.com>
2024-05-20 22:24:44 +0200

    added button icons to menu

--- app/public/_includes/menu.php
+++ app/public/_includes/menu.php
@@ -1,5 +1,5 @@
 <nav id="burger_menu" style="display:none;">
-<button onclick="displayMenu()" id="close-menu">close</button>
+<button onclick="displayMenu()" id="close-menu"><i class="fa-solid fa-xmark fa-2xl"></i></button>
         <ul>
             <li><a href="index.php">HOME</a></li>
             <li><a href="register.php">GET STARTED</a></li>
